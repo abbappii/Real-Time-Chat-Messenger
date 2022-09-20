172e8c51048892ce2eacd67fcb195b1ee2b780db
abbappii <bappi142434@gmail.com>
2022-09-20 13:12:36 +0600

    views urls register

--- MessengerChat/account/urls.py
+++ MessengerChat/account/urls.py
@@ -1,1 +1,8 @@
 
+from django.urls import path 
+
+from .views import register_view
+
+urlpatterns = [ 
+    path('', register_view, name='register'),
+]
--- MessengerChat/account/views.py
+++ MessengerChat/account/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+from account.forms import RegistrationForm
+
+def register_view(request, *args, **kwargs):
+    user = request.user
+
+    if user.is_authenticated:
+        return HttpResponse(f"You are already authenticate as {user.email}")
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(email=email,password=password)
+            login(request, user)
+            destination=kwargs.get("next")
+            if destination:
+                return redirect(destination)
+            return redirect("home")
+    else:
+        form = RegistrationForm()
+    context = {
+        'registration_form':form 
+    }
+    return render(request, 'register.html',context)
