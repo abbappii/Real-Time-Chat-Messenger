214e0c919bc74471cefe6c9502f1a11fd1e88623
abbappii <bappi142434@gmail.com>
2022-09-20 16:33:07 +0600

    password reset - change emali send done

--- MessengerChat/MessengerChat/urls.py
+++ MessengerChat/MessengerChat/urls.py
@@ -8,7 +8,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('chat.urls')),
-    path('account/',include('account.urls')),
+    path('',include('account.urls')),
 ]
 
 if settings.DEBUG:
