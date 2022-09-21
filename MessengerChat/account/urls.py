38828c1f8b94026755ae738154817ed71d71acf6
abbappii <bappi142434@gmail.com>
2022-09-21 17:06:01 +0600

    user account view

--- MessengerChat/account/urls.py
+++ MessengerChat/account/urls.py
@@ -2,10 +2,18 @@
 from django.urls import path 
 from django.contrib.auth import views as auth_views
 
-from .views import register_view, login_user, logout_user
+from .views import (
+    register_view, 
+    login_user, 
+    logout_user,
+    account_view,
+)
 
 urlpatterns = [ 
     path('register/', register_view, name='register'),
+    # path('account/<user_id>/',account_view,name='account'),
+    path('account/<int:id>/',account_view,name='account'),
+
     path('login/',login_user,name='login'),
     path('logout/', logout_user,name='logout'),
 
--- MessengerChat/account/views.py
+++ MessengerChat/account/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate, logout
 
+from account.models import Account
 from account.forms import RegistrationForm, AccountAuthenticationForm
+from django.conf import settings
 
 def register_view(request, *args, **kwargs):
     user = request.user
@@ -72,3 +74,46 @@ def get_redirect_if_exists(request):
 def logout_user(request):
     logout(request)
     return redirect('home')
+
+
+
+def account_view(request,id,*args,**kwargs):
+    """
+	- Logic here is kind of tricky
+		is_self (boolean)
+			is_friend (boolean)
+				-1: NO_REQUEST_SENT
+				0: THEM_SENT_TO_YOU
+				1: YOU_SENT_TO_THEM
+	"""
+    context = {}
+    # user_id = kwargs.get("user_id")
+    try:
+        account=Account.objects.get(id=id)
+    except:
+        return HttpResponse("Something went wrong")
+    
+    if account:
+        context['id'] = account.id
+        context['username']=account.username
+        context['email']=account.email
+        context['profile_image'] = account.profile_image.url
+        context['hide_email'] = account.hide_email
+
+        # template variable 
+        is_self = True
+        is_friend = False
+        user=request.user
+
+        if user.is_authenticated and user != account:
+            is_self = False
+        elif not user.is_authenticated:
+            is_self = False
+
+
+        context['is_self'] = is_self
+        context['is_friend'] = is_friend
+        context['BASE_URL'] = settings.BASE_URL
+
+
+    return render(request,'account/account.html',context)
