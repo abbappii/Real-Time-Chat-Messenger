726c4864698c20f3bb02e73ed1eac949a69f2b98
abbappii <bappi142434@gmail.com>
2022-10-06 22:28:07 +0600

    dec fn req urls with templates

--- MessengerChat/friend/urls.py
+++ MessengerChat/friend/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
-from friend.views import send_friend_request, friend_requests, accept_friend_request,remove_friend
+from friend.views import (
+    send_friend_request, 
+    friend_requests, 
+    accept_friend_request,
+    remove_friend,
+    decline_friend_request
+)
 
 urlpatterns = [ 
     path('send-frnd-req/', send_friend_request, name='send_frnd_req'),
     path('frined-request/<int:id>/', friend_requests, name='friend_request'),
     path('accept-friend-request/<friend_request_id>/',accept_friend_request, name='accept_friend_request'),
     path('remove-friend/', remove_friend, name='remove_friend'),
+    path('decline-frined-request/<friend_request_id>/', decline_friend_request, name='decline_friend_request'),
+
    
 ]
