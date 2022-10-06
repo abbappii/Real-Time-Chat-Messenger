da41ac7b910403664786e33c034a3bb7be65f12c
abbappii <bappi142434@gmail.com>
2022-10-06 21:50:27 +0600

    changes for remove frnd request

--- MessengerChat/friend/urls.py
+++ MessengerChat/friend/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from friend.views import send_friend_request, friend_requests, accept_friend_request
+from friend.views import send_friend_request, friend_requests, accept_friend_request,remove_friend
 
 urlpatterns = [ 
     path('send-frnd-req/', send_friend_request, name='send_frnd_req'),
     path('frined-request/<int:id>/', friend_requests, name='friend_request'),
     path('accept-friend-request/<friend_request_id>/',accept_friend_request, name='accept_friend_request'),
-        
+    path('remove-friend/', remove_friend, name='remove_friend'),
+   
 ]
