eb2e761f498bc2a0feb637124e87f09e8ed5ad72
abbappii <bappi142434@gmail.com>
2022-10-06 11:38:56 +0600

    urls friend req

--- MessengerChat/friend/urls.py
+++ MessengerChat/friend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from friend.views import send_friend_request
+from friend.views import send_friend_request, friend_requests
 
 urlpatterns = [ 
     path('send-frnd-req/', send_friend_request, name='send_frnd_req'),
-    
+    path('frined-request/<int:id>/', friend_requests, name='friend_request'),
+        
 ]
