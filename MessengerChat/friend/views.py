ade980c5d15357baa32d3a88cf48c409904045f2
abbappii <bappi142434@gmail.com>
2022-10-06 19:29:26 +0600

    send frnd req with fetch instead of ajax

--- MessengerChat/friend/views.py
+++ MessengerChat/friend/views.py
@@ -1,9 +1,12 @@
 
 from django.dispatch import receiver
 from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
+
 import json
 from account.models import Account
 from friend.models import FriendRequests, FriendList
+
 # Create your views here.
 
 def friend_requests(request,id):
@@ -26,9 +29,14 @@ def friend_requests(request,id):
 def send_friend_request(request,*args, **kwargs):
     user = request.user
     payload = {}
-
+    
+    body_unicode = request.body.decode('utf-8')
+    receiver_user_id = json.loads(body_unicode)['id']
+    print(type(receiver_user_id))
+    print(receiver_user_id)
+ 
     if request.method == 'POST' and  user.is_authenticated:
-        receiver_user_id = request.POST.get('receiver_user_id')
+        print('rec_id',receiver_user_id)
 
         if receiver_user_id:
             receiver = Account.objects.get(id = receiver_user_id)
@@ -64,5 +72,33 @@ def send_friend_request(request,*args, **kwargs):
 
     else:
         payload['response'] = "You are not authenticated user."
-    return HttpResponse(json.dumps(payload), content_type="application/json")
+    # return HttpResponse(json.dumps(payload), content_type="application/json")
+    return JsonResponse(payload)
+
+
+
+def accept_friend_request(request, *args, **kwargs):
+    context = {}
+
+    user = request.user
+    if request.method == 'GET' and user.is_authenticated:
+        friend_request_id = kwargs.get('friend_request_id')
+        if friend_request_id:
+            friend_request = FriendRequests.objects.get(id=friend_request_id)
+            if friend_request.receiver == user:
+                if friend_request:
+                    friend_request.accept()
+                    context['response'] = "Friend request accepted."
+                else:
+                    context['response'] = "something went wrong."
+            else:
+                context['response'] = "That is not your request to accept."
+        else:
+            context['response'] = "Unable to accept the request."
+    else:
+        context['response'] = "You must authenticate to accept a friend request."
+    
+    return JsonResponse(context)
+
+    # return HttpResponse(request,json.dumps(context), content_type='application/json')
                      
