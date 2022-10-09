3c7cb458194a13c5c48f4a2b932d4816c9861005
abbappii <bappi142434@gmail.com>
2022-10-09 23:13:13 +0600

    routing for websocket

--- MessengerChat/MessengerChat/routing.py
+++ MessengerChat/MessengerChat/routing.py
@@ -1,14 +1,18 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.urls import path
+from django.urls import path, re_path
+from django.core.asgi import get_asgi_application
+from channels.http import AsgiHandler
 
+from public_chat.consumers import PublicChatConsumer
 
 application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
 	'websocket': AllowedHostsOriginValidator(
 		AuthMiddlewareStack(
 			URLRouter([
-					
+					path('public_chat/<room_id>/', PublicChatConsumer.as_asgi()),
 			])
 		)
 	),
