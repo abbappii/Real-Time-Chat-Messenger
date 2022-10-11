b68972ebe9d814dcf1d93ab6d78e40580838b6bd
abbappii <bappi142434@gmail.com>
2022-10-11 19:49:09 +0600

    debug value pass

--- MessengerChat/chat/views.py
+++ MessengerChat/chat/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 from django.conf import settings
-
+DEBUG = True
 def Home(request):
     context = {}
-    context['debug_mode'] = settings.DEBUG
+    context['debug'] = DEBUG
     context['room_id'] = "1"
 
     return render(request,'home.html',context)
